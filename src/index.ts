7cada9bd6d344b489f1eb536d1735e0609ba71c0
Yu Ke <Yu.Ke@autodesk.com>
2018-10-30 23:41:01 +0800

    Add generator

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import DB from './DB';
-import Logger from './Logger';
 import _ from 'lodash';
 import AceidBranchGuidGenerator from './AceidBranchGuidGenerator';
 import TemplateGenerator from './TemplateGenerator';
 import AssetGenerator from './AssetGenerator';
+import Logger from './Logger';
 const logger = Logger.getLogger('main');
 
 const idGenerator = new AceidBranchGuidGenerator(10, 10);
 const templateGenerator = new TemplateGenerator(100);
 const assetGenerator = new AssetGenerator(idGenerator, templateGenerator);
+const columns = ['aceid', 'branch_guid', 'guid', 'name', 'template'];
 
-const paylod = assetGenerator.batchGet(10);
-
-async function test() {
-    const columns = ['aceid', 'branch_guid', 'guid', 'name', 'template'];
-    await DB.insert('assets', paylod, columns);
-    await DB.closeConnection();
+async function generate(seq: number) {
+  logger.info(`Generator ${seq} starts`);
+  const paylod = assetGenerator.batchGet(5);
+  await DB.insert('assets', paylod, columns);
 }
 
-test();
+function runall() {
+  return _.range(5).map((n) => generate(n));
+}
+
+async function main() {
+  await Promise.all(runall());
+  await DB.closeConnection();
+}
+
+main();
